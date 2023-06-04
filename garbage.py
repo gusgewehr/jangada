43e654ea41464aebec8ff6ef1508f41c1d8432fc
GUSTAVO GEWEHR SOARES <52550134+gusgewehr@users.noreply.github.com>
2023-06-04 19:40:02 -0300

    individual sprites for garbage type

--- garbage.py
+++ garbage.py
@@ -1,14 +1,67 @@
 import pygame
+import random
 
 class Garbage(pygame.sprite.Sprite):
+
+   
+
     def __init__(self, start_pos):
         super().__init__()
 
-        self.sheet = pygame.image.load('garbage.webp') #carrega imagem
-        self.sheet.set_clip(pygame.Rect(0, 0, 1002, 692)) #define uma área retangular
+        type_options = ["Eletrônico", "Plástico", "Metal", "Vidro", "Papel", "Orgânico"]
+
+        type = random.choices(type_options)
+
+        self.grab_counter = 0
+
+        self.type = type[0]
+
+        clip_dict  = {
+            "Eletrônico": [pygame.Rect(24,21,95,117),#celular
+                           pygame.Rect(175,20,75,149),#pilha
+                           pygame.Rect(317,40,64,92),#lampada
+                            
+            ],
+            "Plástico": [pygame.Rect(443,24,163,183),#sacola
+                           pygame.Rect(684,30,95,159),#garrafa_amassada
+                           pygame.Rect(875,32,70,101),#garrafa
+                
+                 
+            ],
+            "Metal": [pygame.Rect(1016,47,162,134),#lata
+                           pygame.Rect(1257,53,85,110),#refri
+                           pygame.Rect(1427,71,74,92),#lampada
+                 
+            ],
+            "Vidro": [pygame.Rect(6,300,140,194),#suco
+                           pygame.Rect(251,268,106,233),#vinho
+                           pygame.Rect(435,325,75,108),#lata_amassada
+                 
+            ],
+            "Papel": [pygame.Rect(635,301,172,166),#jornal
+                           pygame.Rect(845,320,175,109),#caixa
+                           pygame.Rect(1085,327,77,92),#papel
+                 
+            ],
+            "Orgânico": [pygame.Rect(1245,289,104,163),#melancia
+                           pygame.Rect(1403,288,137,149),#banana
+                           pygame.Rect(1602,314,79,107),#ovo
+                 
+            ]
+        }
+
+        sprite = random.choice(clip_dict[self.type])
+
+
+        self.sheet = pygame.image.load('garbage.png') #carrega imagem
+        self.sheet.set_clip(sprite)
+        #self.sheet.set_clip(pygame.Rect(0, 0, 1002, 692)) #define uma área retangular
         self.image = self.sheet.subsurface(self.sheet.get_clip()) #pega a área retangular definida e seta como imagem do sprite
-        self.image = pygame.transform.scale(self.image, (1002 / 20, 692 / 20))
+        self.image = pygame.transform.scale(self.image, (sprite.size[0]/2.5, sprite.size[1]/2.5))
         self.rect = self.image.get_rect()
 
         self.rect.x = start_pos[0]
         self.rect.y = start_pos[1]
+
+        
+
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ clock = pygame.time.Clock()
 #grid = cell.Cell("cell")
 
 WATER = (65,159,204)
+WHITE_TEXT = (255,255,255)
 
 raft = raft.Raft()
 all_sprites = pygame.sprite.Group()
@@ -21,23 +22,27 @@ garbage_group = pygame.sprite.Group()
 island_group = pygame.sprite.Group()
 collide_group = pygame.sprite.Group()
 
-gargabe_x = random.sample(range(-10000, 10000), 3000)
-gargabe_y = random.sample(range(-10000, 10000), 3000)
-for i in range(3000):
+gargabe_x = random.sample(range(-1000, 1000), 100)
+gargabe_y = random.sample(range(-1000, 1000), 100)
+for i in range(100):
     g = garbage.Garbage((gargabe_x[i], gargabe_y[i]))
     all_sprites.add(g)
     garbage_group.add(g)
     collide_group.add(g)
 
-islands_x = random.sample(range(-10000, 10000), 25)
-islands_y = random.sample(range(-10000, 10000), 25)
-for i in range(25):
+islands_x = random.sample(range(-1000, 1000), 3)
+islands_y = random.sample(range(-1000, 1000), 3)
+for i in range(3):
     isle = island.Island(f'islands/isle-{i+1}.webp', (islands_x[i], islands_y[i]))
     all_sprites.add(isle)
     island_group.add(isle)
     collide_group.add(isle)
 
 all_sprites.add(raft)
+
+
+
+
 
 pygame.init()
 
@@ -51,7 +56,14 @@ screen = pygame.display.set_mode((screen_width,screen_height), pygame.FULLSCREEN
 camera = camera.Camera(screen_width, screen_height)
 camera.update(raft, screen_width, screen_height)
 
+
+
+
+
+
 def main():
+    points = 0
+    font = pygame.font.SysFont(None, 24)
     
     running = True
 
@@ -73,6 +85,9 @@ def main():
         #screen.blit(island.image, (12, 24))
         for entity in all_sprites:
             screen.blit(entity.image, camera.apply(entity))
+
+        img = font.render("Pontuação: "+str(points), True, WHITE_TEXT)
+        screen.blit(img, (20, 20))
         
         #print(raft.rect.x)
         #print(raft.rect.y)
@@ -81,10 +96,12 @@ def main():
         collision_list = pygame.sprite.spritecollide(raft,collide_group,False)
         for item in collision_list:
             if isinstance(item, island.Island):
-                print('colidiu com uma ilha')
-            elif isinstance(item, garbage.Garbage):
                 item.kill()
-                print('colidiu com um lixo')
+            elif isinstance(item, garbage.Garbage):
+                item.grab_counter += 1
+                if item.grab_counter >= 15:
+                    item.kill()
+                    points += 1            
 
         pygame.display.update()
 
