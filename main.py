73e69d603d0253fdd7647865a5873700e961d02f
eduardohuther <eduardocristiano2011@hotmail.com>
2023-06-09 19:19:39 -0300

    menu

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ Points = points.Points()
 
 WATER = (65,159,204)
 WHITE_TEXT = (255,255,255)
+BLACK_TEXT = (0,0,0)
 
+raft_menu = raft.Raft()
 raft = raft.Raft()
 all_sprites = pygame.sprite.Group()
 garbage_group = pygame.sprite.Group()
@@ -59,25 +61,17 @@ camera.update(raft, screen_width, screen_height)
 
 ui = pygame.image.load('ui.png').convert()
 
-
-
-
-
-
-
-
 def main():
     
-    
     running = True
+    atualScreen = 'menu'
 
     #grid.build_grid(screen)
-       
+    
 
     pygame.display.update()
 
     while running:
-        screen.fill(WATER)
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
@@ -86,43 +80,70 @@ def main():
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 print("posição do mouse: "+str(event.pos))
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                if atualScreen == 'menu':
+                    atualScreen = 'guide'
 
+        if atualScreen == 'menu':
+            menu()
+            raft_menu.update('stand_right')
+        elif atualScreen == 'guide':
+            screen.fill(WATER)
+        elif atualScreen == 'loading':
+            screen.fill(WATER)
+        else:
+            screen.fill(WATER)
+            raft.handle_event(event)
+            camera.update(raft, screen_width, screen_height)
         
-        raft.handle_event(event)
-        camera.update(raft, screen_width, screen_height)
-        
-        #screen.blit(island.image, (12, 24))
-        for entity in all_sprites:
-            screen.blit(entity.image, camera.apply(entity))
-        
-        
-        #print(raft.rect.x)
-        #print(raft.rect.y)
-        #grid.handle_event(screen, event)
-        
-        collision_list = pygame.sprite.spritecollide(raft,collide_group,False)
-        for item in collision_list:
-            if isinstance(item, island.Island):
-                item.item_drop_timer += 1
-                if item.item_drop_timer >= 25:                    
-                    Points.decrese_points(item.type)
-                    item.item_drop_timer = 0
-            elif isinstance(item, garbage.Garbage):
-                item.grab_counter += 1
-                if item.grab_counter >= 15:
-                    item.kill()
-                    Points.add_points(item.type)
+            #screen.blit(island.image, (12, 24))
+            for entity in all_sprites:
+                screen.blit(entity.image, camera.apply(entity))
         
         
+            #print(raft.rect.x)
+            #print(raft.rect.y)
+            #grid.handle_event(screen, event)
         
-        screen.blit(ui, (0,0)) 
-        Points.print_points_on_screen(screen)    
+            collision_list = pygame.sprite.spritecollide(raft,collide_group,False)
+            for item in collision_list:
+                if isinstance(item, island.Island):
+                    item.item_drop_timer += 1
+                    if item.item_drop_timer >= 25:                    
+                        Points.decrese_points(item.type)
+                        item.item_drop_timer = 0
+                elif isinstance(item, garbage.Garbage):
+                    item.grab_counter += 1
+                    if item.grab_counter >= 15:
+                        item.kill()
+                        Points.add_points(item.type)
+            screen.blit(ui, (0,0)) 
+            Points.print_points_on_screen(screen)    
         
         pygame.display.update()
 
         clock.tick(60)
         
     pygame.quit()
+
+def menu():
+    screen.fill(WATER)
+    menu_reft = raft_menu.image.get_rect()
+    menu_reft.midtop = (screen_width/2, 300)
+    screen.blit(raft_menu.image, menu_reft)
+    draw_text("JANGADA", 95, screen_width/2, 100, BLACK_TEXT)
+    draw_text("POLUIÇÃO NAS ÁGUAS", 15, screen_width/2, 200, BLACK_TEXT)
+    draw_text("PRESSIONE [ENTER] PARA INICIAR", 30, screen_width/2, screen_height/1.5, BLACK_TEXT)
+    draw_text("PRESSIONE [ESC] PARA SAIR", 25, screen_width/2, (screen_height/1.5) + 50, BLACK_TEXT)
+    draw_text("Eduardo Hüther, Gustavo Gewehr", 12, screen_width/2, screen_height - 50, BLACK_TEXT)
+
+def draw_text(text, size, x, y, color):
+    '''draw text to screen'''
+    font = pygame.font.Font(pygame.font.match_font('arial'), size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    screen.blit(text_surface, text_rect)
 
 if __name__=="__main__":
     # call the main function
